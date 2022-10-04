5670fe1b504d420b504b98442e1c2034dc9791bc
Tommy <t.a.britnell@gmail.com>
2022-10-04 15:43:35 +0200

    filter search

--- src/notes/userselect.jsx
+++ src/notes/userselect.jsx
@@ -3,10 +3,31 @@ import { useQuery } from "@tanstack/react-query";
 import { getUsers } from "./api";
 import { Combobox } from "@headlessui/react";
 
+const queryFilter = (query, users) => {
+  // add search score to user data
+  const userScores = users.map((user) => {
+    const score = [user.first_name, user.last_name, user.email, user.username]
+      .map((str) => str.includes(query))
+      .reduce((a, b) => a + b, 0);
+    return {
+      ...user,
+      score,
+    };
+  });
+  // sort & filter by score
+  return userScores
+    .sort((a, b) => {
+      if (a.score > b.score) return -1;
+      if (a.score < b.score) return 1;
+      return 0;
+    })
+    .slice(0, 5);
+};
 export default function UserSelect({ selectedUser, setSelectedUser }) {
   const { data: users } = useQuery(["users"], getUsers);
 
-  const filtered = users.slice(0, 5);
+  const [query, setQuery] = useState("");
+  const filtered = queryFilter(query, users);
 
   // TODO filter
 
@@ -24,7 +45,7 @@ export default function UserSelect({ selectedUser, setSelectedUser }) {
             <Combobox.Option key={user.username} value={user}>
               {({ active, selected }) => (
                 <li className={"py-1 px-2" + (active ? " bg-blue-200 " : "")}>
-                  {user.username}
+                  {user.username} - {user.first_name} {user.last_name}
                 </li>
               )}
             </Combobox.Option>
